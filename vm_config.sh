bfa463174cbca2473a1208473abd5a1e48ef137e
PierrickRauby <pierrick.rauby@gadz.org>
2018-11-22 10:51:51 -0500

    vm_config working fine

--- vm_config.sh
+++ vm_config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash                                                                     
 #script to configure the vm on glcoud
-# first let this scrip be exectutable 
-#       chmod +x config.sh
 # to answer prompt automatically use  the following command line to start the script
-#       yes Y |./config.sh
+#       yes Y | sh config.sh
+echo 'updating apt-get'
+sudo apt-get update
 echo 'installing git'
 sudo apt-get install git
 echo 'installing pip3'
@@ -11,19 +11,21 @@ sudo apt-get install python3-pip
 echo 'install libsm6 libxext6 libxrender-dev'
 sudo apt-get install -y libsm6 libxext6 libxrender-dev
 echo 'installing numpy tensorflow and keras'
-sudo pip3 install numpy keras tensorflow pillow matplotlib opencv-python
+sudo pip3 install numpy keras tensorflow pillow matplotlib 
+sudo pip3 install opencv-python
 echo 'cloning https://github.com/PierrickRauby/keras-yolo3.git'
 sudo git clone https://github.com/PierrickRauby/keras-yolo3.git
 echo '################################################### entering keras-yolo3/ ###################################################'
-alias proj="cd keras-yolo3/"
-proj
+alias cd_folder='cd keras-yolo3'
+cd_folder
 echo 'loading weight'
-wget https://pjreddie.com/media/files/yolov3.weights
+sudo wget https://pjreddie.com/media/files/yolov3.weights
 echo 'converting weight, destination model_data/yolo.h5'
-python3 convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
+# alias command_weight='python3 convert.py yolov3.cfg yolov3.weights model_data/yolo.h5'
+sudo python3 convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
 echo 'copying video'
-gsutil cp gs://cse-6242-project/2018_10_17_0900_2130_C3.mp4 .
+sudo gsutil cp gs://cse-6242-project/2018_10_14_0900_2200_C4.mp4 .
 echo '-----'
 echo 'starting analysis'
-python3 yolo_video.py --input 2018_10_17_0900_2130_C3.mp4 --frame_ratio 0.00006 --json_config_path C3_config.json --visual_display 0
+sudo python3 yolo_video.py --input 2018_10_14_0900_2200_C4.mp4 --frame_ratio 0.1 --json_config_path C4_config.json --visual_display 0
 echo 'done'
